cc52759c87b575789b3ef28de1f57252b6df24fb
Robin Schroer <sulami@peerwire.org>
2019-04-22 12:47:10 +0200

    Add some explorative experiments

--- src/neko/core.clj
+++ src/neko/core.clj
@@ -17,9 +17,15 @@
                  {:default-to-write false
                   :keymap
                   {\i #(switch-mode :insert)
+                   \: #(switch-mode :command)
                    \q #(System/exit 0)}}
 
                  :insert
+                 {:default-to-write true
+                  :keymap
+                  {:escape #(switch-mode :normal)}}
+
+                 :command
                  {:default-to-write true
                   :keymap
                   {:escape #(switch-mode :normal)}}}}))
@@ -77,7 +83,84 @@
 (defn -main
   "Let's get this kitty started."
   [& args]
-  (let [t (term/get-terminal :swing)]
+  (let [t (term/get-terminal :swing)] ;; also: text
     (swap! global-state assoc-in [:terminal] t)
     (term/start t)
     (input-loop t)))
+
+
+;; exploration
+(def buffer {:content (list
+                       "line one"
+                       "line two"
+                       "line three")
+             :file nil})
+
+(def window {:buffer buffer
+             :position 1
+             :formatters (list
+                          #(map clojure.string/capitalize %))
+             :cursor [0 0]})
+
+(defn content->markup [content]
+  ;; TODO define a flexible markup format which formatters can work on
+  content)
+
+(defn apply-formatters [window]
+  (let [formatters (:formatters window)]
+    (update-in window
+               [:buffer :content]
+               (fn [x] (reduce #(%2 %1) x formatters)))))
+
+(defn trim-window-content [window]
+  (let [pos (:position window)]
+    (update-in window [:buffer :content] #(drop pos %))))
+
+;; Idea
+;; Have standardised classes of tokens
+;; Tag things with their "class" of colour
+;; Have the frontend use colourschemes
+;; Have a standardised format for schemes
+
+(-> window
+    (update-in [:buffer :content] content->markup)
+    int
+    apply-formatters
+    trim-window-content) ; then pass to interface
+
+;; Formatters as actions
+;; we fork the buffer content for each formatter to work on (eventually off-process)
+;; then merge them into one
+;; ordering should be arbitrary
+;; formatters return actions
+;; actions are data (should probably have specs)
+;; actions are limited so that conflicts are avoided. so no "rewrite this line"
+;; intermediate format is still tbd
+;; TODO: figure out how to prevent conflicts, eg. what happens if two insertions happen in the same place? which order will they be in?
+
+(defn run-formatters [base formatters]
+  (let [actions (map #(% base) formatters)]
+    (apply concat actions)))
+
+(defn insert-header-formatter [content]
+  (list {:action :insert
+         :position 0
+         :content ["# header"]}))
+
+(defn insert-footer-formatter [content]
+  (list {:action :insert
+         :position (count content)
+         :content ["-- footer"]}))
+
+(defn apply-action [base action]
+  (case (:action action)
+
+    :insert
+    (concat (take (:position action) base)
+            (:content action)
+            (drop (:position action) base))))
+
+(let [base ["abc"]]
+  (->> (run-formatters base [insert-header-formatter
+                             insert-footer-formatter])
+       (map (partial apply-action base))))
